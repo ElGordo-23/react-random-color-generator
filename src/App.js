f7370c98429305ec05452c22dff691e6541491f9
Felix Lang <felix.lang@mailbox.org>
2021-09-14 14:23:33 +0200

    Make first running version.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import React, { useState } from 'react';
 export default function App() {
   const [color, setColor] = useState(randomColor());
 
-  console.log(color);
-
   return (
     <div className="App">
-      <h1 style={{}}> Le Random Color Generator</h1>
+      <h1
+        className="Headline"
+        style={{
+          color: color,
+          marginTop: 30,
+        }}
+      >
+        Le Random Color Generator
+      </h1>
       <div
         className="colorBox"
         style={{
@@ -24,14 +30,29 @@ export default function App() {
           left: '50%',
           top: '50%',
           transform: 'translate(-50%, -50%)',
+          marginTop: 30,
         }}
       />
       <br />
       <br />
       <br />
       <br />
-      <div>{randomColor()}</div>
-      <button onClick={() => setColor(randomColor.randomColor())}>
+      <div
+        style={{
+          padding: 10,
+          fontFamily: 'Helvetica',
+        }}
+      >
+        {randomColor()}
+      </div>
+      <button
+        style={{
+          width: 150,
+          fontStyle: 'italic',
+          fontWeight: 'bold',
+        }}
+        onClick={() => setColor(randomColor.randomColor())}
+      >
         Generate!
       </button>
     </div>
